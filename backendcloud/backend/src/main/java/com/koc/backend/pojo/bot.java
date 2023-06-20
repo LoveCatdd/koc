07ae6370369111197fe17ac37c746ecdf2f681b7
楓谷 <90328048+SilverwingN-EX@users.noreply.github.com>
2023-06-20 20:08:05 +0800

    完成后端骨架， 完成登录、注册api

---
+++ backendcloud/backend/src/main/java/com/koc/backend/pojo/bot.java
@@ -0,0 +1,23 @@
+package com.koc.backend.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class bot {
+    private int id;
+    private int userId;
+    private char title;
+    private char description;
+    private char content;
+    private int rating;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createtime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date modifytime;
+}
